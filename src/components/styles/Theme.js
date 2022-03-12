734b76eafa720d0c308011e5763ffb73df6325e7
rjlacanlaled <rjlacanlaled@gmail.com>
2022-03-12 21:04:14 +0800

    added theme

---
+++ src/components/styles/Theme.js
@@ -0,0 +1,34 @@
+const theme =  {
+    colors: {
+        body: {
+            fontColorLight: "black",
+            fontColorDark: "white",
+            backgroundColorLight: "white",
+            backgroundColorDark: "gray"
+        },
+        primaryButton: {
+            fontColorLight: "black",
+            fontColorDark: "white",
+            backgroundColorLight: "white",
+            backgroundColorDark: "black",
+            borderColorLight: "white",
+            borderColorDark: "black",
+            hoverFontColorLight: "gray",
+            hoverFontColorDark: "white",
+            hoverBackgroundColorLight: "black",
+            hoverBackgroundColorDark: "white",
+            hoverBorderColorLight: "black",
+            hoverBorderColorDark: "white"
+        },
+        secondaryButton: {
+
+        },
+        primaryInput: {
+
+        },
+        secondaryInput: {
+            
+        }
+    }
+}
+
