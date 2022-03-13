fd0650cae6e34e550cf030a51c257fb37bb863ba
Eren <94876850+KuroEren@users.noreply.github.com>
2022-03-13 17:09:04 +0800

    Initail Sidebar Commit

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import './App.css';
 import { ThemeProvider } from 'styled-components';
 import { theme } from './components/styles/Theme';
 import GlobalStyles from './components/styles/Global';
+import SideBar from './features/sidebar/Sidebar';
 
 export default function App() {
     return (
         <ThemeProvider theme={theme}>
             <GlobalStyles />
-            <div className='App'></div>
+            <div className='App'>
+                <SideBar />
+            </div>
         </ThemeProvider>
     );
 }
