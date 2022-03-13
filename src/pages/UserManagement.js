0247a8f2d790f7495fe3bfea32a5e9b40664cd52
rjlacanlaled <rjlacanlaled@gmail.com>
2022-03-13 13:52:10 +0800

    added pages folder

---
+++ src/pages/UserManagement.js
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+export default function UserManagement(props) {
+    return (
+        <Wrapper>
+
+        </Wrapper>
+
+    );
+}
+
+const Wrapper = styled.div`
+
+
+
+`;
