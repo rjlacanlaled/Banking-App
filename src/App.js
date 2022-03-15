90d190d4cf62fdc3f08fad4605665c037ba5f897
Eren <94876850+KuroEren@users.noreply.github.com>
2022-03-15 16:46:36 +0800

    added fixes to app.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,26 @@
-import './App.css';
-import { ThemeProvider } from 'styled-components';
+import styled, { ThemeProvider } from 'styled-components';
 import { theme } from './components/styles/Theme';
 import GlobalStyles from './components/styles/Global';
-import SideBar from './components/Sidebar';
-import SendReceiveMoney from './components/SendReceiveMoney';
+import UserManagement from './pages/UserManagement';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 export default function App() {
     return (
-        <ThemeProvider theme={theme}>
-            <GlobalStyles />
-            <div className='App'>
-                <SendReceiveMoney />
-            </div>
-        </ThemeProvider>
+        <BrowserRouter>
+            <ThemeProvider theme={theme}>
+                <GlobalStyles />
+                <Main>
+                    <Routes>
+                        <Route path='/' />
+                        <Route path='/login' />
+                        <Route path='/users' element={<UserManagement />} />
+                        <Route path='/transactions' />
+                    </Routes>
+                </Main>
+            </ThemeProvider>
+        </BrowserRouter>
     );
 }
+
+const Main = styled.div`
+`;
