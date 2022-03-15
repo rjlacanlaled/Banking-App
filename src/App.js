04080969f1924530bcc306324c33a11a8cfa7069
rjlacanlaled <rjlacanlaled@gmail.com>
2022-03-15 16:53:16 +0800

    added login to routes

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { theme } from './components/styles/Theme';
 import GlobalStyles from './components/styles/Global';
 import UserManagement from './pages/UserManagement';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Login from './pages/Login';
 
 export default function App() {
     return (
@@ -12,7 +13,7 @@ export default function App() {
                 <Main>
                     <Routes>
                         <Route path='/' />
-                        <Route path='/login' />
+                        <Route path='/login' element={<Login />}/>
                         <Route path='/users' element={<UserManagement />} />
                         <Route path='/transactions' />
                     </Routes>
