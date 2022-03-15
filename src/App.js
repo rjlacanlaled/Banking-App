add9656785d71e34dc22ab1cf25fc72c8b776b20
rjlacanlaled <rjlacanlaled@gmail.com>
2022-03-15 16:29:06 +0800

    added routes

--- src/App.js
+++ src/App.js
@@ -3,16 +3,22 @@ import { ThemeProvider } from 'styled-components';
 import { theme } from './components/styles/Theme';
 import GlobalStyles from './components/styles/Global';
 import UserManagement from './pages/UserManagement';
-import AddUser from './components/AddUser';
-import AddUserConfirmation from './components/AddUserConfirmation';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 export default function App() {
     return (
-        <ThemeProvider theme={theme}>
-            <GlobalStyles />
-            <div className='App'>
-              <UserManagement />
-            </div>
-        </ThemeProvider>
+        <BrowserRouter>
+            <ThemeProvider theme={theme}>
+                <GlobalStyles />
+                <div className='App'>
+                    <Routes>
+                      <Route path="/login" /> 
+                      <Route path="/users" element={<UserManagement />}/>
+                      <Route path="/transactions" /> 
+                      <Route path="/" />
+                    </Routes>
+                </div>
+            </ThemeProvider>
+        </BrowserRouter>
     );
 }
