e9c574ae379803460fc933b16bb82636b3d259a0
Eren <94876850+KuroEren@users.noreply.github.com>
2022-03-15 16:43:22 +0800

    app modifications

--- src/App.js
+++ src/App.js
@@ -3,13 +3,14 @@ import { ThemeProvider } from 'styled-components';
 import { theme } from './components/styles/Theme';
 import GlobalStyles from './components/styles/Global';
 import SideBar from './components/Sidebar';
+import SendReceiveMoney from './components/SendReceiveMoney';
 
 export default function App() {
     return (
         <ThemeProvider theme={theme}>
             <GlobalStyles />
             <div className='App'>
-                <SideBar />
+                <SendReceiveMoney />
             </div>
         </ThemeProvider>
     );
