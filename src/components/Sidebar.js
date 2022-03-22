ac93242b10bd1462550adbaee357f52f6ad59e6e
rjlacanlaled <rjlacanlaled@gmail.com>
2022-03-22 15:08:02 +0800

    updated styled

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -5,62 +5,81 @@ import { NegativeButton } from './styles/Buttons.styled';
 import { MdOutlineDashboard } from 'react-icons/md';
 import { GrUserManager } from 'react-icons/gr';
 import { RiLuggageDepositLine } from 'react-icons/ri';
-import { AiOutlineTransaction } from 'react-icons/ai';
+import { AiOutlineTransaction, AiOutlineDoubleLeft, AiOutlineDoubleRight, AiOutlineLogout } from 'react-icons/ai';
 import useAuth from './hooks/useAuth';
 import useActivePage from './hooks/useActivePage';
 
 export default function SideBar() {
+    const [collapsed, setCollapsed] = useState(false);
     const auth = useAuth();
     const activePage = useActivePage();
 
+    const handleSidebarCollapse = () => {
+        setCollapsed(!collapsed);
+    };
+
     return (
-        <Container>
-            <Logo src='https://scontent.fmnl30-1.fna.fbcdn.net/v/t1.15752-9/275554048_4912356165478076_1490016745173868883_n.jpg?_nc_cat=103&ccb=1-5&_nc_sid=ae9488&_nc_eui2=AeHl2MrrXaIHZIxN82OQY7xQzFNPBHk-t4PMU08EeT63g84Sw1-8D53mUTyWrqI4Jj2A1KThHyFiQlxTzQV3RSbK&_nc_ohc=eso_EihlyCUAX_cBbO2&_nc_ht=scontent.fmnl30-1.fna&oh=03_AVJA9tVuSY9e5CyQogwRVPn2VdccZDlPlSALknU4nCt_6g&oe=62508466' />
+        <Container collapsed={collapsed}>
+            <Logo
+                src='https://scontent.fmnl30-1.fna.fbcdn.net/v/t1.15752-9/275554048_4912356165478076_1490016745173868883_n.jpg?_nc_cat=103&ccb=1-5&_nc_sid=ae9488&_nc_eui2=AeHl2MrrXaIHZIxN82OQY7xQzFNPBHk-t4PMU08EeT63g84Sw1-8D53mUTyWrqI4Jj2A1KThHyFiQlxTzQV3RSbK&_nc_ohc=eso_EihlyCUAX_cBbO2&_nc_ht=scontent.fmnl30-1.fna&oh=03_AVJA9tVuSY9e5CyQogwRVPn2VdccZDlPlSALknU4nCt_6g&oe=62508466'
+                collapsed={collapsed}
+            />
             <PageList>
                 <PageItem>
-                    <MdOutlineDashboard />
-                    <StyledLink to='/' page='dashboard' active={activePage}>
+                    <StyledMdOutlineDashboard active={activePage} page='dashboard' />
+                    <StyledLink to='/' page='dashboard' active={activePage} collapsed={collapsed}>
                         Dashboard
                     </StyledLink>
                 </PageItem>
                 <PageItem>
-                    <RiLuggageDepositLine />
-                    <StyledLink to='/transact' page='transact' active={activePage}>
+                    <StyledRiLuggageDepositLine active={activePage} page='dashboard' />
+                    <StyledLink to='/transact' page='transact' active={activePage} collapsed={collapsed}>
                         Transact
                     </StyledLink>
                 </PageItem>
                 <PageItem>
-                    <GrUserManager />
-                    <StyledLink to='/users' page='users' active={activePage}>
+                    <StyledGrUserManager active={activePage} page='dashboard' />
+                    <StyledLink to='/users' page='users' active={activePage} collapsed={collapsed}>
                         Manage Users
                     </StyledLink>
                 </PageItem>
                 <PageItem>
-                    <AiOutlineTransaction />
-                    <StyledLink to='/transactions' page='transaction-history' active={activePage}>
+                    <StyledAiOutlineTransaction active={activePage} page='dashboard' />
+                    <StyledLink to='/transactions' page='transaction-history' active={activePage} collapsed={collapsed}>
                         Transaction History
                     </StyledLink>
                 </PageItem>
             </PageList>
             <ButtonContainer>
-                <StyledNegativeButton onClick={auth.logout}>Log out</StyledNegativeButton>
-                <ToggleSwitch type='checkbox' />
+                {collapsed ? (
+                    <StyledAiOutlineLogout onClick={auth.logout} />
+                ) : (
+                    <StyledNegativeButton onClick={auth.logout}>Log out</StyledNegativeButton>
+                )}
+                <ToggleSwitch type='checkbox' collapsed={collapsed} />
+                {collapsed ? (
+                    <StyledAiOutlineDoubleRight onClick={handleSidebarCollapse} />
+                ) : (
+                    <StyledAiOutlineDoubleLeft onClick={handleSidebarCollapse} />
+                )}
             </ButtonContainer>
         </Container>
     );
 }
 
 const Container = styled.div`
+
     display: flex;
     flex-direction: column;
     align-items: center;
     justify-content: space-between;
-    min-width: 200px;
+    min-width: ${({ collapsed }) => (collapsed ? '10px' : '200px')};
     padding: 20px;
 `;
 const Logo = styled.img`
-    width: 100px;
-    height: 100px;
+    visibility: ${({ collapsed }) => (collapsed ? 'hidden' : 'visible')};
+    width: ${({ collapsed }) => (collapsed ? '0px' : '100px')};
+    height: ${({ collapsed }) => (collapsed ? '0px' : '100px')};
 `;
 const PageList = styled.ul`
     display: flex;
@@ -121,9 +140,9 @@ const Switch = styled.input`
     }
 `;
 
-const ToggleSwitch = () => {
+const ToggleSwitch = ({ collapsed }) => {
     return (
-        <ToggleLabel>
+        <ToggleLabel collapsed={collapsed}>
             <Switch type='checkbox' />
             <Slider />
         </ToggleLabel>
@@ -131,13 +150,79 @@ const ToggleSwitch = () => {
 };
 
 const ToggleLabel = styled.label`
-    width: 100px;
+    width: ${({ collapsed }) => (collapsed ? '0px' : '100px')};
     height: 34px;
     position: relative;
     margin-top: 5px;
+    visibility: ${({ collapsed }) => (collapsed ? 'hidden' : 'visible')};
 `;
 
 const StyledLink = styled(Link)`
-    color: ${({ active: { active }, page, theme }) => (active == page ? theme.colors.main.themeColor: 'black')};
+    color: ${({ active: { active }, page, theme }) => (active == page ? theme.colors.main.themeColor : 'black')};
     transition: all 0.25s ease;
+    display: ${({ collapsed }) => (collapsed ? 'none' : 'block')};
+`;
+
+const StyledAiOutlineDoubleLeft = styled(AiOutlineDoubleLeft)`
+    cursor: pointer;
+
+    &:hover {
+        color: ${({theme}) => theme.colors.main.themeColor};
+    }
+`;
+
+const StyledAiOutlineDoubleRight = styled(AiOutlineDoubleRight)`
+    cursor: pointer;
+
+    &:hover {
+        color: ${({theme}) => theme.colors.main.themeColor};
+    }
+`;
+
+const StyledAiOutlineLogout = styled(AiOutlineLogout)`
+    cursor: pointer;
+
+    color: red;
+
+    &:hover {
+        color: ${({theme}) => theme.colors.main.themeColor};
+    }
+`;
+
+const StyledMdOutlineDashboard = styled(MdOutlineDashboard)`
+    cursor: pointer;
+
+    color: ${({ active: { active }, page, theme }) => (active == page ? theme.colors.main.themeColor : 'black')};
+
+    &:hover {
+        color: ${({theme}) => theme.colors.main.themeColor};
+    }
+`;
+const StyledRiLuggageDepositLine = styled(RiLuggageDepositLine)`
+    cursor: pointer;
+
+    color: ${({ active: { active }, page, theme }) => (active == page ? theme.colors.main.themeColor : 'black')};
+
+    &:hover {
+        color: ${({theme}) => theme.colors.main.themeColor};
+    }
+`;
+const StyledGrUserManager = styled(GrUserManager)`
+    cursor: pointer;
+
+    color: ${({ active: { active }, page, theme }) => (active == page ? theme.colors.main.themeColor : 'black')};
+
+    &:hover {
+        color: ${({theme}) => theme.colors.main.themeColor};
+    }
+`;
+
+const StyledAiOutlineTransaction = styled(AiOutlineTransaction)`
+    cursor: pointer;
+
+    color: ${({ active: { active }, page, theme }) => (active == page ? theme.colors.main.themeColor : 'black')};
+
+    &:hover {
+        color: ${({theme}) => theme.colors.main.themeColor};
+    }
 `;
--- src/pages/TransactionPage.js
+++ src/pages/TransactionPage.js
@@ -1,167 +1,159 @@
-import React, { useState, useEffect } from "react";
-import styled from "styled-components";
-import { TransactionTypes } from "../model/enums/transaction-types";
-import Deposit from "../components/Deposit";
-import Withdraw, { userId } from "../components/Withdraw";
-import Transfer from "../components/Transfer";
-import { Modal } from "../components/styles/Modal.styled";
-import useActivePage from "../components/hooks/useActivePage";
+import React, { useState, useEffect } from 'react';
+import styled from 'styled-components';
+import { TransactionTypes } from '../model/enums/transaction-types';
+import Deposit from '../components/Deposit';
+import Withdraw, { userId } from '../components/Withdraw';
+import Transfer from '../components/Transfer';
+import { Modal } from '../components/styles/Modal.styled';
+import useActivePage from '../components/hooks/useActivePage';
+import { PageTitle, PageTitleContainer } from '../components/styles/Titles.styled';
 
 const TRANSACTIONTYPESLIST = Object.values(TransactionTypes);
 
 export default function MakeATransaction({ bank }) {
-   const [showModal, setShowModal] = useState(false)
-   const [transactionType, setTransactionType] = useState(
-      TRANSACTIONTYPESLIST[2]
-   );
-   const activePage = useActivePage();
+    const [showModal, setShowModal] = useState(false);
+    const [transactionType, setTransactionType] = useState(TRANSACTIONTYPESLIST[2]);
+    const activePage = useActivePage();
 
-   useEffect(() => {
-       activePage.setActive('transact');
-   }, []);
+    useEffect(() => {
+        activePage.setActive('transact');
+    }, []);
 
-   
+    const handleTransaction = e => {
+        const { value } = e.target;
+        setTransactionType(value);
+        console.log(bank.users);
+    };
 
-   const handleTransaction = (e) => {
-      const { value } = e.target;
-      setTransactionType(value);
-      console.log(bank.users);
-   };
 
-   return (
-      <MainContainer>
-         <Modal show={showModal} />
-         <FirstBox>
-            <BoxTitle>Make a Transaction</BoxTitle>
-            <BoxAction>
-               <BoxOptions value={transactionType} onChange={handleTransaction}>
-                  {TRANSACTIONTYPESLIST.map((option) => {
-                     return (
-                        <option value={option}>{option.toUpperCase()}</option>
-                     );
-                  })}
-               </BoxOptions>
-            </BoxAction>
-         </FirstBox>
+
+    return (
+        <MainContainer>
+            <PageTitleContainer>
+                <PageTitle>Transact</PageTitle>
+            </PageTitleContainer>
+            <Modal show={showModal} />
+            <FirstBox>
+                <BoxTitle>Make a Transaction</BoxTitle>
+                <BoxAction>
+                    <BoxOptions value={transactionType} onChange={handleTransaction}>
+                        {TRANSACTIONTYPESLIST.map(option => {
+                            return <option value={option}>{option.toUpperCase()}</option>;
+                        })}
+                    </BoxOptions>
+                </BoxAction>
+            </FirstBox>
             {transactionType === TRANSACTIONTYPESLIST[2] ? (
-               <Deposit bank={bank} />
+                <Deposit bank={bank} />
             ) : transactionType === TRANSACTIONTYPESLIST[1] ? (
-               <Withdraw bank={bank} show={setShowModal} />
+                <Withdraw bank={bank} show={setShowModal} />
             ) : (
-               <Transfer bank={bank} show={setShowModal}/>
+                <Transfer bank={bank} show={setShowModal} />
             )}
-         
-      </MainContainer>
-   );
+        </MainContainer>
+    );
 }
 
 export const BoxContainer = styled.div`
-   width: 40%;
-   height: 90px;
-   margin-bottom: 20px;
-   margin-right: 20%;
-   box-shadow: 0 0 10px rgb(136, 136, 136);
-   background-color: rgb(0, 191, 255);
+    width: 40%;
+    height: 90px;
+    margin-bottom: 20px;
+    margin-right: 20%;
+    box-shadow: 0 0 10px rgb(136, 136, 136);
+    background-color: rgb(0, 191, 255);
 `;
 
 const FirstBox = styled(BoxContainer)`
-   margin-top: 10%;
-   `
+    margin-top: 10%;
+`;
 
 export const BoxTitle = styled.h3`
-   width: 100%;
-   height: 50%;
-   padding: 10px 0 10px 30px;
+    width: 100%;
+    height: 50%;
+    padding: 10px 0 10px 30px;
 `;
 
 export const BoxAction = styled.div`
-   width: 100%;
-   height: 50%;
-   display: flex;
-   align-items: center;
+    width: 100%;
+    height: 50%;
+    display: flex;
+    align-items: center;
 `;
 
 export const BoxOptions = styled.select`
-   width: 50%;
-   margin: 0 0 5% 20%;
-   outline: none;
-   padding: 2px 3px;
-   border-radius: 3px;
-   background-image: linear-gradient(
-      rgba(0, 0, 0, 0.2),
-      rgba(255, 255, 255, 0.5)
-   );
+    width: 50%;
+    margin: 0 0 5% 20%;
+    outline: none;
+    padding: 2px 3px;
+    border-radius: 3px;
+    background-image: linear-gradient(rgba(0, 0, 0, 0.2), rgba(255, 255, 255, 0.5));
 `;
 
 export const SubmitContainer = styled.div`
-   width: 10%;
-   
-   
-   margin-bottom: 0;
-   margin-right: 20%;
-   box-shadow: none;
-   display: flex;
-   justify-content: center;
+    width: 10%;
+
+    margin-bottom: 0;
+    margin-right: 20%;
+    box-shadow: none;
+    display: flex;
+    justify-content: center;
 `;
 
 export const SubmitButton = styled.button.attrs(({ type }) => ({
-   type: "submit",
+    type: 'submit',
 }))`
-   width: 90%;
-   height: 100%;
-   padding: 10%;
+    width: 90%;
+    height: 100%;
+    padding: 10%;
 `;
 
 export const Form = styled.form`
-   width: 100%;
-   height: 100%;
+    width: 100%;
+    height: 100%;
 
-   display: flex;
-   flex-direction: column;
-   align-items: center;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
 `;
 
 export const AmountInput = styled.input.attrs(({ type }) => ({
-   type: type || "number",
+    type: type || 'number',
 }))`
-   width: 50%;
-   margin: 0 0 5% 20%;
-   outline: none;
-   padding: 2px 3px;
-   border-radius: 3px;
-   border: 1px solid black;
-   background-image: linear-gradient(
-      rgba(0, 0, 0, 0.2),
-      rgba(255, 255, 255, 0.5)
-   );
-   &::-webkit-outer-spin-button,
-   &::-webkit-inner-spin-button {
-      -webkit-appearance: none;
-      margin: 0;
-   }
+    width: 50%;
+    margin: 0 0 5% 20%;
+    outline: none;
+    padding: 2px 3px;
+    border-radius: 3px;
+    border: 1px solid black;
+    background-image: linear-gradient(rgba(0, 0, 0, 0.2), rgba(255, 255, 255, 0.5));
+    &::-webkit-outer-spin-button,
+    &::-webkit-inner-spin-button {
+        -webkit-appearance: none;
+        margin: 0;
+    }
 `;
 
 const MainContainer = styled.div`
-   display: flex;
-   flex-direction: column;
-   align-items: center;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
 
-   height: 100vh;
-   width: 100vw;
+    background-color: ${({ theme }) => theme.colors.main.themeColor};
+
+    height: 100vh;
+    width: 100vw;
 `;
 
-
 export const TransactionSuccess = styled.div`
-   width: 30%;
-   height: 10%;
+    width: 30%;
+    height: 10%;
 
-   position: absolute;
-   left: 40%;
-   transform: translateY(-50%);
+    position: absolute;
+    left: 40%;
+    transform: translateY(-50%);
 
-   display: ${({ showTransactionSuccessModal }) =>
-      showTransactionSuccessModal ? "flex" : "none"};
-   justify-content: center;
-   align-items: center;
-   background-color: red;
+    display: ${({ showTransactionSuccessModal }) => (showTransactionSuccessModal ? 'flex' : 'none')};
+    justify-content: center;
+    align-items: center;
+    background-color: red;
 `;
